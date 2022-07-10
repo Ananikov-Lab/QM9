33f494a3d21b8dfdc8244f3cda3590fb9b9965cb
Nikita Kolomoets <xfait1234567890@gmail.com>
2022-07-10 03:18:26 +0300

    some changes

--- final_filter/fine_filter_reactions_by_energy.py
+++ final_filter/fine_filter_reactions_by_energy.py
@@ -3,13 +3,12 @@ import pickle as pkl
 import pandas as pd
 import numpy as np
 
-def filter_reactions(input_csv):
+def filter_reactions(input_csv, n, output_csv):
     need_reactions = []
-    df = pd.read_csv(input_csv)
-    df.sort_values(by=['gibbs energy'], inplace=True, ascending=False).iloc[:10]
-    
-    for row in df.rows:
-        need_reactions.append((row['reag'], row['alkyne'], row['prod']))
+    df = pd.read_csv(input_csv, sep=';')
+    df.sort_values(by=['gibbs energy'], inplace=True, ascending=True)
+    df_need = df.iloc[:n]
+    df_need.to_csv(output_csv)
 
     return need_reactions
 
@@ -20,11 +19,10 @@ if __name__ == '__main__':
     parser.add_argument('--input-csv', dest='csv', type=str, 
                        help='csv file with calculated reactions')
     parser.add_argument('--output', dest='output_need_reactions', type=str, 
-                       help='output path of need reactions for experiments')
+                       help='output path csv of need reactions for experiments')
+    parser.add_argument('--number', dest='number', type=int, 
+                       help='number of need reactions')
     args = parser.parse_args()
     
-    reactions_for_expetiments = filter_reactions(args.input_csv)
-   
-    with open(args.output, 'wb') as f:
-        pkl.dump(reactions_for_experiment, f)
+    filter_reactions(args.csv, args.number, args.output_need_reactions)
         
